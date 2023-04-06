3485c8dc17bbef984f42b63eb0fc0efba6624598
Anh Quan <quan52188@donga.edu.vn>
2023-04-06 17:03:07 +0700

    Delete src directory

--- src/main/java/com/example/appname/repositories/LinkRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.appname.repositories;
-
-import com.example.appname.models.Link;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LinkRepo extends CrudRepository<Link, Integer> {
-    Iterable<Link> findAllByUserId(int userId);
-}
--- src/main/java/com/example/appname/services/ReceivedDataServices.java
+++
@@ -1,9 +0,0 @@
-package com.example.appname.services;
-
-import java.util.List;
-
-import com.example.appname.models.Role;
-
-public interface ReceivedDataServices {
-    List<Role> findAll();
-}
